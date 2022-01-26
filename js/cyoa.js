afe96632a77ad572ca7640b90215ae566e4496d0
pytrik <pytrik.elzinga@gmail.com>
2022-01-26 21:24:42 +0100

    choice gets initialized and routing works

--- js/cyoa.js
+++ js/cyoa.js
@@ -1,22 +1,35 @@
-class CYOA {
-  constructor(onIdChanged, choiceElement, choices) {
-    this.choices = choices;
-    this.choiceElement = choiceElement;
-    this.#setupListener(onIdChanged);
-  }
+const CYOA = function CYOA(onIdChanged, choiceElement, choices) {
+  const self = this;
+  self.choices = choices;
+  self.choiceElement = choiceElement;
+  self.choicesDict = choices.reduce((prev, curr) => {
+    prev[curr.id] = curr;
+    return prev;
+  } , {})
 
-  #setupListener(onIdChanged) {
-    onIdChanged(this.render);
-  }
-
-  render(id) {
+  self.render = function(id) {
+    id = decodeURIComponent(id);
     CYOA.log(`id set to ${id}`);
-    // todo: actually render a thing
+
+    const title = self.choiceElement.getElementsByClassName("title")[0];
+    const body = self.choiceElement.getElementsByClassName("body")[0];
+    const options = self.choiceElement.getElementsByClassName("options")[0];
+
+    const choice = self.choicesDict[id];
+
+    title.innerHTML = choice.title;
+    body.innerHTML = choice.body;
+    options.innerHTML = (choice.options || []).map(createChoiceOption).join("\n");
   }
 
-  static log(msg) {
-    console.log(`${new Date().toISOString()} - ${CYOA.name}: ${msg}`);
+  const createChoiceOption = (option) => {
+    return `<li><a href="#${encodeURIComponent(option.id) || ""}">${option.name || option.id}</a></li>`;
   }
+
+  const setupListener = (onIdChanged) => onIdChanged(self.render);
+  setupListener(onIdChanged);
 }
+
+CYOA.log = (msg) => console.log(`${new Date().toISOString()} - ${CYOA.name}: ${msg}`);
 
 CYOA.log("scripts loaded");
--- js/init.js
+++ js/init.js
@@ -4,29 +4,10 @@ const onIdChanged = (cb) => {
 
 const choiceElement = document.getElementById("current-choice");
 
-const cyoa = new CYOA(onIdChanged, choiceElement, [
-  {
-    id: "001-start",
-    title: "Welkom!",
-    text: "Dit is het begin van dit kies-je-eigen-avontuur",
-    options: [
-      { id: "002-nope-im-out", name: "NOPE!" },
-      { id: "003-yes-lets-go", name: "Let's GOOOO!" }
-    ]
-  }, {
-    id: "002-nope-im-out",
-    title: "Nope I'm out!",
-    text: "kthnxbye"
-  }, {
-    id: "003-yes-lets-go",
-    title: "Yes! Let's go!",
-    text: "Cool :)",
-    options: [
-      { id: "005-id-only-test" },
-      { name: "Name only test" },
-      {}
-    ]
-  }
-]);
+const cyoa = new CYOA(onIdChanged, choiceElement, choices);
+
+const hash = document.location.hash.substring(1) || "001-start";
+document.location.hash = `#${hash}`;
+cyoa.render(hash);
 
 CYOA.log("initialized");
