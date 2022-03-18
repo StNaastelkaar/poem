1c79a7c9d0d31b57023af7068272651a9536ba1d
leazael <leazael@gmail.com>
2022-03-18 23:22:06 +0100

    asd

---
+++ js/choices_example.js
@@ -0,0 +1,29 @@
+const choices = [
+  { id: "undefined",
+    title: "You dun goofed",
+    body: "'t huidige id is 'undefined', je hebt dus iets verkeerd gedaan..."
+  },
+  {
+    id: "001-start",
+    title: "Welkom!",
+    body: "Dit is het begin van dit cooloe <br> kies-je-eigen-avontuur",
+    options: [
+      { id: "002-nope-im-out", name: "NOPE!" },
+      { id: "003-yes-lets-go", name: "Let's GOOOO!" }
+    ]
+  }, {
+    id: "002-nope-im-out",
+    title: "Nope I'm out!",
+    body: "kthnxbye"
+  }, {
+    id: "003-yes-lets-go",
+    title: "Yes! Let's go!",
+    body: "Cool :)",
+    options: [
+      { id: "005-id-only-test" },
+      { name: "Name only test" },
+      { id: "001-start" },
+      { id: "003-yes-lets-go" }
+    ]
+  }
+]
