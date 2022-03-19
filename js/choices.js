8ee47029667a2c0718a12804510427005ee3b8da
leazael <leazael@gmail.com>
2022-03-19 16:05:38 +0100

    asd

--- js/choices.js
+++ js/choices.js
@@ -3,46 +3,46 @@ const choices = [
     title: "You dun goofed",
     body: "'t huidige id is 'undefined', je hebt dus iets verkeerd gedaan..."
   }, {
-    id: "START",
-    title: "WELKOM NIEUWE SPELER",
+    id: "START", title: "WELKOM NIEUWE SPELER",
     body: "Toen Darius dit start scherm zach, klikte hij snel op...",
     options: [
       { id: "intro-001", name: "START!" }
     ]
   }, {
-    id: "intro-001",
-    title: "TUTORIAL",
+    id: "intro-001", title: "TUTORIAL",
     body: "Toen Darius dit spel tot zich nam, <br> schrok hij zich een hoedje-lam. <br> Twee spannende keuze die hij daar voor zich zag,",
     options: [
       { id: "intro-002", name: "En er verscheen bij hem een brede lach!" },
       { id: "intro-003", name: "Daarvan raakte hij toch wat van slag!" }
     ]
   }, {
-    id: "intro-002",
-    title: "",
+    id: "intro-002", title: "",
     body: "\"Dit is leuk\", zij hij, \"dit is uniek\"! <br> Vertelde hij aan zijn publiek. <br> En nog voor Darius zich kon vervelen,",
     options: [
       { id: "intro-004", name: "begon gij al snel het spel te spelen." }
     ]
   }, {
-    id: "intro-003",
-    title: "",
+    id: "intro-003", title: "",
     body: "Hij naam een diepe ademteug en bedaarde snel,",
     options: [
       { id: "intro-004", name: "en begon met goede moed aan zijn nieuwe spel." }
     ]
   }, {
-    id: "intro-004",
-    title: "",
-    body: "Oh, de spanning, oh de mogelijkheden, <br> maar waar zou hij zijn playthrough aan besteden? <br> \"Ik weet\", zij Darius, \"wat ik wil gamen!\"",
+    id: "intro-004", title: "",
+    body: "Oh! De spanning! Oh! de mogelijkheden! <br> Maar waar zou hij zijn playthrough aan besteden? <br> \"Ik weet\", zij Darius, \"wat ik wil gamen!\"",
     options: [
-      { id: "serius-001", name: "Een uitdagend spel, vol raadsels en problemen!" },
-      { id: "play-001", name: "Een ontspannen spel, dat ik rustig kan ondernemen." },
+      { id: "serius-001", name: "\"Een uitdagend spel, vol raadsels en problemen!\"" },
+      { id: "play-001", name: "\"Een ontspannen spel, dat ik rustig kan ondernemen!\"" },
     ]
   }, {
-    id: "serius-001",
-    title: "TBC",
-    body: ""
+    id: "serius-001", title: "",
+    body: "Ja, van een moeijlijk spel, dat valt Darius wel goed.",
+    options: [
+      { id: "serius-002", name: "Dus aan de slag met goede moed!" },
+    ]
+  }, {
+    id: "serius-002", title: "",
+    body: "\"Maar wacht toch eens een momentje even\", <br> dacht Darius, \"ben ik mijn doel niet voorbij aan het streven?\" <br> Een gedachte ontname hem wat van zijn stoom <br> Want <i>hit<\i> zo'n molijk spel niet wat te <i>close to home<\i>?"
   }, {
     id: "play-001",
     title: "TBC",
